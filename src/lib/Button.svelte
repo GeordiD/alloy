52750fc397828ef51a5f3498cb82657174c618d4
GeordiD <geordi.dosher@gmail.com>
2022-03-07 21:01:32 -0600

    Rolled back storybook to non-alpha version, svelte stories working

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,3 +1,7 @@
+<script>
+	export let variant = 'primary';
+</script>
+
 <button class="button bg-yellow-500 px-3 py-1 rounded-full text-white" on:click>
-	<slot />
+	<slot>Default Button</slot>
 </button>
