18498b170100e6e4ca510de7cea6f899563ad0ed
GeordiD <geordi.dosher@gmail.com>
2022-03-07 21:16:02 -0600

    Tailwind working in Storybook preview

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,6 +1,9 @@
 const preprocess = require('svelte-preprocess');
 
 module.exports = {
+	core: {
+		builder: 'storybook-builder-vite'
+	},
 	stories: ['../src/lib/*.stories.svelte'],
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
 	framework: '@storybook/svelte',
